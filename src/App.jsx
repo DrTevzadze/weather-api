e9c9a951321cd8e229f948530b7724e60f57e6b5
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-10-25 17:18:46 +0400

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import "./App.css";
 import axios from "axios";
 import WeatherApp from "./components/WeatherApp";
 import apiKey from "../secret";
+import { useState, useEffect } from "react";
 
 const baseUrl = "https://api.openweathermap.org/data/2.5/";
 
 function App() {
+  const fetchApi = async () => {
+    const response = await fetch(`${baseUrl}weather?q=london&appid=${apiKey}`);
+    try {
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>Hello All</h1>
+      <button onClick={fetchApi}>Check API</button>
       <WeatherApp />
     </div>
   );
