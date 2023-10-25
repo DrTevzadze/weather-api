b88beb797313556db48bc7cf5129558d93957eba
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-10-25 16:19:51 +0400

    Secure apiKey and update .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import "./App.css";
 import axios from "axios";
 import WeatherApp from "./components/WeatherApp";
+import apiKey from "../secret";
 
+const baseUrl = "https://api.openweathermap.org/data/2.5/";
 
 function App() {
-
-  
-
   return (
     <div>
       <h1>Hello All</h1>
       <WeatherApp />
     </div>
- )
-};
+  );
+}
 
 export default App;
