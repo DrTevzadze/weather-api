5802bfee44400a0baffcd139fa900c48ff2ff755
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-10-25 17:17:31 +0400

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import axios from "axios";
-import WeatherApp from "./components/WeatherApp";
 import apiKey from "../secret";
 import { useState, useEffect } from "react";
 
@@ -10,7 +9,7 @@ function App() {
   const fetchApi = async () => {
     try {
       const response = await axios.get(
-        "https://api.openweathermap.org/data/2.5/weather?q=london&appid=3720679616f1bfccdf200adb5db3dca7"
+        `${baseUrl}weather?q=london&appid=${apiKey}`
       );
       if (response.status === 200) {
         const jsonResponse = await response.data;
@@ -22,10 +21,15 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Hello All</h1>
-      <button onClick={fetchApi}>Check API</button>
-      <WeatherApp />
+    <div className="main-container">
+      <div className="searchBar">
+        <input
+          type="text"
+          placeholder="Search for a city..."
+          style={{ fontStyle: "italic" }}
+        />
+        <button onClick={fetchApi}>Search</button>
+      </div>
     </div>
   );
 }
