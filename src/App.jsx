8c2f52a48eacccff311a6a9e7444964ffa2df0b8
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-10-25 15:38:02 +0400

    Add a new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import WeatherApp from "./components/WeatherApp";
+
 
 function App() {
 
   return (
     <div>
       <h1>Hello All</h1>
+      <WeatherApp />
     </div>
  )
 };
